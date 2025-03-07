bcdd02db662c08af9e659e1297b03ab289b6f210
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-03-06 19:03:13 -0500

    Mensajes en consola

--- ED_refuerzo/Program.cs
+++ ED_refuerzo/Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Universidad Estatal Amazónica"); //Console.Writeline sirve para escribir textos en pantalla
+System.Console.WriteLine("==============================");
+
+string estudiante = "Carlos Muñoz";
+
+System.Console.WriteLine("Bienvenido " + estudiante + ", estas en tu casa " + estudiante);
+System.Console.WriteLine("Bienvenido {0}, estas en tu casa {1}", estudiante, estudiante);
+System.Console.WriteLine($"Bienvenido {estudiante}, estas en tu casa {estudiante}");
