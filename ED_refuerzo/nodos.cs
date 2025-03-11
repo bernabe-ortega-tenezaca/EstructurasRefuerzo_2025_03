72824bdad3724c04c466f57f4121e8cb80272d18
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-03-11 18:32:21 -0500

    Definición de nodo de tipo strings

---
+++ ED_refuerzo/nodos.cs
@@ -0,0 +1,11 @@
+public class Nodo{
+    public string nombres;
+    public Nodo izquierdo;
+    public Nodo derecho;
+
+    public Nodo(string nombre){
+        nombre = nombre;
+        izquierdo = null;
+        derecho = null;
+    }
+}
