8d1a6e1da4465c09774f28a9ad9b148ece137d3d
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-03-15 08:22:21 -0500

    clase nodo y clase arbol

--- ED_refuerzo/Program.cs
+++ ED_refuerzo/Program.cs
@@ -1,5 +1,44 @@
 ﻿Console.WriteLine("Universidad Estatal Amazónica"); //Console.Writeline sirve para escribir textos en pantalla
 Console.WriteLine("=============================="); //Llamar al método Writeline de la clase Console.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Diccionarios1.run();
 //Diccionarios2.run();
 
@@ -14,19 +53,19 @@ Console.WriteLine("=============================="); //Llamar al método Writeli
 //     System.Console.WriteLine("no es igual");
 // }
 
-ArbolbinarioBusqueda misEstudiantes = new ArbolbinarioBusqueda();
-// juan, ana, antonio, juanita, pepe, lucia
-misEstudiantes.insertarEstudiante("Juan"); // Juan se convierte en la raíz
-misEstudiantes.insertarEstudiante("Anita");
-misEstudiantes.insertarEstudiante("Antonio");
+// ArbolbinarioBusqueda misEstudiantes = new ArbolbinarioBusqueda();
+// // juan, ana, antonio, juanita, pepe, lucia
+// misEstudiantes.insertarEstudiante("Juan"); // Juan se convierte en la raíz
+// misEstudiantes.insertarEstudiante("Anita");
+// misEstudiantes.insertarEstudiante("Antonio");
 
-bool encontrado = misEstudiantes.Buscar("juan");
+// bool encontrado = misEstudiantes.Buscar("juan");
 
-if(encontrado){
-    System.Console.WriteLine("encontrado");
-}else{
-    System.Console.WriteLine("No existe este registro");
-}
+// if(encontrado){
+//     System.Console.WriteLine("encontrado");
+// }else{
+//     System.Console.WriteLine("No existe este registro");
+// }
 
 
 
---
+++ ED_refuerzo/claseArbol.cs
@@ -0,0 +1,7 @@
+public class Arbol{
+    public Node raiz; //ver diapositiva 9 semana 13
+
+    //TODO: Método de insercíon de datos
+    //TODO: Eliminación de un nodo
+    //TODO: Búsqueda
+}
