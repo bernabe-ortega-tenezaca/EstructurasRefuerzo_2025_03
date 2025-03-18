6117f284ab7ed390adaff9cba72520355f5cf0ba
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-03-18 18:36:31 -0500

    Definicion de arbol

---
+++ arbol.cs
@@ -0,0 +1,11 @@
+public class Arbol{
+    public Nodo raiz;
+
+    //TODO: insertar
+    //TODO: eliminar
+    //TODO: buscar
+    //TODO: recorrer
+    //TODO: contar
+    //TODO: getNivelMaximo
+    //TODO: getNoHojas
+}
